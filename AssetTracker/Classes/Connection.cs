bec386c991bcabc133458ac011a709f7b9f3a510
Scott Robinson <scott0robinson0@gmail.com>
2022-10-07 16:18:40 +0100

    Add Classes project Add Connection class

---
+++ AssetTracker/Classes/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Classes
+{
+    public class Connection
+    {
+        public MySqlConnection conn;
+
+        public void Connect()
+        {
+            conn = new MySqlConnection("Server=lochnagar.abertay.ac.uk; Database=sql2203326; Uid=sql2203326; Pwd=iVGGteQzELna;");
+        }
+    }
+}
