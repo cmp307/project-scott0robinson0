125adf71f643fa895a53f15a0d3a0144eceb5cc3
Scott Robinson <scott0robinson0@gmail.com>
2022-10-08 14:12:54 +0100

    Add CRUD functions

--- AssetTracker/AssetTracker/Form1.cs
+++ AssetTracker/AssetTracker/Form1.cs
@@ -9,17 +9,26 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using Classes;
-using MySqlX.XDevAPI.CRUD;
 
 namespace AssetTracker
 {
     public partial class Form1 : Form
     {
         DataTable dataTable;
+        MySqlDataAdapter adapter;
         public Form1()
         {
             InitializeComponent();
-
+            try
+            {
+                //CRUD.Insert("user_", "firstname, lastname, username, email, password", "'first1', 'last1', 'username1', 'email1', 'password1'");
+                //CRUD.DeleteRowByPK("user_", "userid = 36");
+                //CRUD.UpdateRowByPK("user_", "firstname = 'new name'", "userid = 37");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
@@ -27,9 +36,9 @@ namespace AssetTracker
             try
             {
                 dataTable = new DataTable();
+                adapter = new MySqlDataAdapter(CRUD.SelectAllFromTable("user_"));
                 adapter.Fill(dataTable);
                 dataGridView1.DataSource = dataTable.DefaultView;
-                connection.Conn.Close();
             }
             catch (Exception ex)
             {
--- AssetTracker/Classes/CRUD.cs
+++ AssetTracker/Classes/CRUD.cs
@@ -8,19 +8,51 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    internal class CRUD
+    public static class CRUD
     {
-        public MySqlDataAdapter SelectAllFromTable(string tableName)
+        public static MySqlCommand SelectAllFromTable(string tableName)
         {
             Connection connection = new Connection();
             MySqlCommand command;
-            MySqlDataAdapter adapter;
             connection.Connect();
             connection.Conn.Open();
-            command = new MySqlCommand("SELECT * FROM item", connection.Conn);
+            command = new MySqlCommand("SELECT * FROM " + tableName, connection.Conn);
             command.ExecuteNonQuery();            
-            adapter = new MySqlDataAdapter(command);
-            return adapter;
+            connection.Conn.Close();
+            return command;
+        }
+
+        public static void Insert(string tableName, string columns, string values)
+        {
+            Connection connection = new Connection();
+            MySqlCommand command;
+            connection.Connect();
+            connection.Conn.Open();
+            command = new MySqlCommand("INSERT INTO " + tableName + " (" + columns + ") VALUES (" + values + ")", connection.Conn);
+            command.ExecuteNonQuery();
+            connection.Conn.Close();
+        }
+
+        public static void DeleteRowByPK(string tableName, string condition)
+        {
+            Connection connection = new Connection();
+            MySqlCommand command;
+            connection.Connect();
+            connection.Conn.Open();
+            command = new MySqlCommand("DELETE FROM " + tableName + " WHERE " + condition, connection.Conn);
+            command.ExecuteNonQuery();
+            connection.Conn.Close();
+        }
+
+        public static void UpdateRowByPK(string tableName, string values, string condition)
+        {
+            Connection connection = new Connection();
+            MySqlCommand command;
+            connection.Connect();
+            connection.Conn.Open();
+            command = new MySqlCommand("UPDATE " + tableName + " SET " + values + " WHERE " + condition, connection.Conn);
+            command.ExecuteNonQuery();
+            connection.Conn.Close();
         }
     }
 }
